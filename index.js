edfd873466290252223e775cdab02efc39e0d607
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-06-22 13:45:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+let employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let updatedEmployee={...employee};
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee;
+}
